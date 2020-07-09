6f2187dabfbc6a1ef6e023db84ee66b4d7fd6a46
Erika Yardumian <eyard@erikas-mbp>
2020-07-09 14:12:25 -0700

    Initial Commit

---
+++ Single-component Picker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Single-component Picker
+//
+//  Created by Erika Yardumian on 7/9/20.
+//  Copyright © 2020 Erika Yardumian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
